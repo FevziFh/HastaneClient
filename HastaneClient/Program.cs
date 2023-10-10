55a176c1a4de858de7bc9fe4ffeef6fb349b4033
FevziFh <49607200+FevziFh@users.noreply.github.com>
2023-10-10 12:31:03 +0300

    asd

--- HastaneClient/Program.cs
+++ HastaneClient/Program.cs
@@ -9,6 +9,14 @@ namespace HastaneClient
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            //2.yöntem
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
+            });
+            //3.Yöntem
+            builder.Services.AddCors(options=>options.AddPolicy("myclient",opt=>opt.WithOrigins("https://localhost:7166","").SetIsOriginAllowedToAllowWildcardSubdomains().AllowAnyHeader().AllowAnyMethod()));
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -20,6 +28,17 @@ namespace HastaneClient
             }
 
             app.UseHttpsRedirection();
+
+            //1.Yöntem
+            //app.UseCors(options =>
+            //{
+            //    options.AllowAnyHeader().AllowAnyHeader().AllowAnyMethod();
+            //});
+            //app.UseCors();
+
+            app.UseCors(options=>options.AllowAnyOrigin());
+
+            app.UseCors("myClient");
             app.UseStaticFiles();
 
             app.UseRouting();
